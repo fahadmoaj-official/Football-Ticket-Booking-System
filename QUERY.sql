faa2f9bb728e2d7f60881cdf6be98231ac7911f7
Fahad Sarker <moajfahad@gmail.com>
2026-06-27 11:52:41 +0600

    Add & problems solves & chages some

--- QUERY.sql
+++ QUERY.sql
@@ -4,13 +4,13 @@
 
 DROP TABLE IF EXISTS Bookings;
 DROP TABLE IF EXISTS Matches;
-DROP TABLE IF EXISTS Users0;
+DROP TABLE IF EXISTS Users;
 
 -- =========================================================================
 -- 1. CREATE USERS TABLE
 -- =========================================================================
 
-CREATE TABLE Users0 (
+CREATE TABLE Users (
     user_id SERIAL PRIMARY KEY,
     full_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
@@ -46,7 +46,7 @@ CREATE TABLE Matches (
 CREATE TABLE Bookings (
     booking_id SERIAL PRIMARY KEY,
 
-    user_id INTEGER NOT NULL REFERENCES Users0(user_id),
+    user_id INTEGER NOT NULL REFERENCES Users(user_id),
 
     match_id INTEGER NOT NULL REFERENCES Matches(match_id),
 
@@ -72,7 +72,7 @@ CREATE TABLE Bookings (
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO USERS
 -- =========================================================================
-INSERT INTO Users0(user_id, full_name, email, role, phone_number) VALUES
+INSERT INTO Users(user_id, full_name, email, role, phone_number) VALUES
 (1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
 (2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
 (3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
@@ -100,3 +100,64 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 
 
 
+
+-- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+
+SELECT match_id,fixture,base_ticket_price from Matches
+WHERE tournament_category ='Champions League'
+     and match_status ='Available'
+
+
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+
+SELECT user_id,full_name,email from Users
+WHERE full_name ILIKE 'Tanvir%' 
+     or full_name ILIKE '%Haque%'
+
+
+-- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+
+
+select booking_id,user_id,match_id, COALESCE(payment_status,'Action Required') as systematic_status
+FROM Bookings
+WHERE payment_status is null;
+  
+
+-- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams;
+
+select b.booking_id,u.full_name,m.fixture,b.total_cost 
+from bookings as b 
+INNER join Matches as m 
+on m.match_id = b.match_id
+INNER join Users as u  
+on u.user_id = b.user_id  ;
+
+
+
+-- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
+
+
+select u.user_id,u.full_name,b.booking_id 
+from Users as u 
+left join bookings as b 
+on u.user_id = b.user_id  
+left join Matches as m  
+on m.match_id = b.match_id;
+
+
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+
+
+SELECT booking_id, match_id, total_cost
+FROM bookings
+WHERE total_cost > (
+    SELECT AVG(total_cost)
+    FROM bookings
+);
+
+
+-- Query 7: Retrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match.
+
+select match_id,fixture,base_ticket_price from Matches
+ORDER BY base_ticket_price DESC
+LIMIT 2 OFFSET 1
